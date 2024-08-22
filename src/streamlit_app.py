f63e22afd6a603934202df25bcedebb5f48fc3ba
Anoif01 <evol954746456@gmail.com>
2024-08-22 02:37:04 +0200

    Updated videos

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -18,7 +18,7 @@ import config
 # Streamlit app title
 st.title("Motion Mapper： a pose estimation and trajectory tracking app")
 
-UPLOAD_DIRECTORY = "/assets/"
+UPLOAD_DIRECTORY = os.path.join(os.getcwd(), 'assets') # change to your path where save the videos
 
 # File uploader
 uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov"])
@@ -37,18 +37,20 @@ if st.button("Start Processing"):
 
         # update config
         config.video_path = file_path
+        config.resize_video_path = os.path.join(UPLOAD_DIRECTORY, 'resize_'+filename)
         output_video_path = os.path.join(UPLOAD_DIRECTORY, 'results', 'output_'+filename)
         config.output_video_path = output_video_path
+        print(config.video_path, config.resize_video_path, config.output_video_path)
 
         # Display a processing message
-        st.write("Processing... Please wait, this may take a while.")
+        st.write("Processing... Please wait, this may take a while (10 mins for 15 seconds video). Do not interrupt the processing.")
 
         # Run the main function with the updated config
         main(config)
         
         # ffmpeg command convert to H.264
-        h264_output_video_path = "output_h264_video.mp4"
-        ffmpeg_command = f"ffmpeg -i {output_video_path} -vcodec libx264 {h264_output_video_path}"
+        h264_output_video_path = filename+"_h264_video.mp4"
+        ffmpeg_command = f"ffmpeg -y -i {output_video_path} -vcodec libx264 {h264_output_video_path}"
 
         # execute ffmpeg
         subprocess.run(ffmpeg_command, shell=True)
@@ -61,3 +63,6 @@ if st.button("Start Processing"):
             st.error("Failed to convert video to H.264 format.")
     else:
         st.write("Please upload a video file to start processing.")
+        
+        
+ 
